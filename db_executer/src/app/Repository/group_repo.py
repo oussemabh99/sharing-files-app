1623ad1409109894432f8ca49404b2912d5c4895
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2025-12-22 21:33:43 +0100

    add service group

--- db_executer/src/app/Repository/group_repo.py
+++ db_executer/src/app/Repository/group_repo.py
@@ -1,6 +1,6 @@
 from app.Models.models import Role,Group,GroupRole
 from app.Extensions.db import SessionLocal
-def group_roles(name : str) -> list[Role]:
+def get_group_roles(name : str) -> list[Role]:
     session = SessionLocal()
     roles = session.query(Role.name).filter(Role.uuid==GroupRole.role_uuid).filter(Group.uuid==GroupRole.group_uuid).filter(Group.name==name).all()
     session.close()
---
+++ db_executer/src/app/Services/group.py
@@ -0,0 +1,7 @@
+import app.Repository.group_repo as grp_repo
+import app.Repository.role_repo as role_repo
+def get_permission_from_group_service(name : str) -> set:       
+    roles = grp_repo.get_group_roles(name)
+    all_permissions = { permission[0] for role in roles for permission in role_repo.get_permission_roles(role[0])}
+    print(all_permissions)
+
