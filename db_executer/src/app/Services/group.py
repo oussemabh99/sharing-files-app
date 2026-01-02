b1d0abe15382edc36010e158273737f80963de9d
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2026-01-02 18:51:49 +0100

    add try -except

--- db_executer/src/app/Services/group.py
+++ db_executer/src/app/Services/group.py
@@ -1,7 +1,11 @@
 import app.Repository.group_repo as grp_repo
 import app.Repository.role_repo as role_repo
-def get_permission_from_group_service(name : str) -> set:       
-    roles = grp_repo.get_group_roles(name)
-    all_permissions = { permission[0] for role in roles for permission in role_repo.get_permission_roles(role[0])}
-    return all_permissions
+def get_permission_from_group_service(name : str) -> set: 
+    try :      
+     roles = grp_repo.get_group_roles(name)
+     all_permissions = { permission[0] for role in roles for permission in role_repo.get_permission_roles(role[0])}
+     return all_permissions
+    except Exception as e:
+       raise e
+    
 
--- db_executer/src/app/Services/user.py
+++ db_executer/src/app/Services/user.py
@@ -3,17 +3,26 @@ import app.Repository.role_repo as role_repo
 import app.Services.group as grp
 def get_user_permission_service(name : str) -> set :
     all_permissions = set()
-    roles = user_repo.get_user_role(name)
-    all_permissions = { permission[0] for role in roles for permission in role_repo.get_permission_roles(role[0])}
-    return all_permissions
+    try :
+     roles = user_repo.get_user_role(name)
+     all_permissions = { permission[0] for role in roles for permission in role_repo.get_permission_roles(role[0])}
+     return all_permissions
+    except Exception as e:
+       raise e
 def get_user_groups_permissions(name : str) -> set :
     all_permissions = set()
-    groups = user_repo.get_user_groups(name)
-    for group in groups :
+    try :
+     groups = user_repo.get_user_groups(name)
+     for group in groups :
        all_permissions=grp.get_permission_from_group_service(group[0]).union(all_permissions)
-    return all_permissions
+     return all_permissions
+    except Exception as e:
+       raise e
 def user_update_service(name : str)-> set :
-    user_permission = get_user_permission_service(name)
-    user_group = get_user_groups_permissions(name)
-    return user_permission.union(user_group)
+    try :
+     user_permission = get_user_permission_service(name)
+     user_group = get_user_groups_permissions(name)
+     return user_permission.union(user_group)
+    except Exception as e:
+       raise e
 
